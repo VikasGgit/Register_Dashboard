d8846b634792146dd6771750b2a3ec45cf81ecc5
Vikas Gupta <150171585+VikasGgit@users.noreply.github.com>
2024-07-22 07:09:50 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -39,7 +39,7 @@ const Sidebar = () => {
 
     const handleLogout= async()=>{
     
-         await axios.get("http://localhost:4000/api/v2/admin/logout" ,{withCredentials: true})
+         await axios.get("https://register-backend-r8qp.onrender.com/api/v2/admin/logout" ,{withCredentials: true})
          .then((res)=>{
             toast.success(res.data.message)
             setIsAuthenticated(false);
